2e2a4fc5b67fd27ce81d6cac83f2a2758f1bede9
Mohammed diedhiou <mohammed.diedhiou@gmail.com>
2025-07-20 21:39:10 +0200

    Add chat-memory with database

--- src/main/java/dev/guava/client_support/AssistantController.java
+++ src/main/java/dev/guava/client_support/AssistantController.java
@@ -2,8 +2,11 @@ package dev.guava.client_support;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +15,17 @@ public class AssistantController {
 
     private final ChatClient chatClient;
 
-    public AssistantController(ChatClient.Builder chatClient, ChatMemory chatMemory) {
+    public AssistantController(ChatClient.Builder chatClient, PromptChatMemoryAdvisor promptChatMemoryAdvisor) {
         this.chatClient = chatClient
-            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+            .defaultAdvisors(promptChatMemoryAdvisor)
             .build();
     }
 
-    @GetMapping("/assistant")
-    public String assistant(@RequestParam String question) {
+    @GetMapping("{user}/assistant")
+    public String assistant(@PathVariable("user") String user, @RequestParam String question) {
         return chatClient.prompt()
             .user(question)
+            .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, user))
             .call()
             .content();
     }
--- src/main/java/dev/guava/client_support/ClientSupportApplication.java
+++ src/main/java/dev/guava/client_support/ClientSupportApplication.java
@@ -1,11 +1,15 @@
 package dev.guava.client_support;
 
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ClientSupportApplication {
@@ -15,10 +19,19 @@ public class ClientSupportApplication {
 	}
 
     @Bean
-    public ChatMemory chatMemory() {
-        return MessageWindowChatMemory
+    PromptChatMemoryAdvisor promptChatMemoryAdvisor(DataSource dataSource) {
+        var jdbc = JdbcChatMemoryRepository
             .builder()
-            .maxMessages(100)
+            .dataSource(dataSource)
+            .build();
+
+        var chatMessageWindow = MessageWindowChatMemory
+            .builder()
+            .chatMemoryRepository(jdbc)
+            .build();
+
+        return PromptChatMemoryAdvisor
+            .builder(chatMessageWindow)
             .build();
     }
 
